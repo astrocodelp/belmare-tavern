11b0f47b37b3702a4fbd92c4a624a7132ed3f245
Vasileios Drosatos <vasilis@decode.gr>
2025-10-07 12:08:05 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,20 +10,42 @@ export default function Footer() {
           <div className="col-md-12">
             <div className="mu-footer-area">
               <div className="mu-footer-social">
-                <a href="/">
+                <a
+                  href="https://www.facebook.com/people/BelMare-Dafni-Beach/100084849824977/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <span className="fa fa-facebook"></span>
                 </a>
-                <a href="/">
+                <a
+                  href="https://www.instagram.com/explore/locations/1163437757040170/bel-mare-dafni-beach/?hl=en"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <span className="fa fa-instagram"></span>
                 </a>
               </div>
               <div className="mu-footer-copyright">
                 <p>
                   &copy; {t("copyright")}{" "}
-                  <a rel="nofollow" href="http://ormosagency.gr">
+                  <a
+                    href="http://ormosagency.gr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     ormosagency.gr
                   </a>
                   . All right reserved.
+                </p>
+                <p>
+                  Powered by rel="nofollow"
+                  <a
+                    href="https://astrocode.gr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    astrocode.gr
+                  </a>
                 </p>
               </div>
             </div>
